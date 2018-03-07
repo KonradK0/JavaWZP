d4d09ca2d3fed1c79753fce3cf97bc0f7dc50cd5
Konrad Korus <konrad.korus0@gmail.com>
2018-03-07 23:51:53 +0100

    first commit

---
+++ Zajecia1/gradle-example/src/main/java/GradleExample.java
@@ -0,0 +1,7 @@
+import org.apache.commons.lang3.math.Fraction;
+
+public class GradleExample {
+    public static void main(String[] args) {
+        System.out.println(Fraction.ONE_FIFTH);
+    }
+}
---
+++ Zajecia1/gradle-filter-example/src/main/java/GradleFilterExample.java
@@ -0,0 +1,6 @@
+public class GradleFilterExample {
+
+    public static void main(String[] args) {
+
+    }
+}
